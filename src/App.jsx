1084a74465de86f3f16054091362b1f60562f576
1967cooder <silvanalindholm@hotmail.com>
2026-02-05 12:23:25 +0200

    Browser router changed with Hash router in App.jsx for easy deploying in GitHub pages, insaled react-router.dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // import "./App.css";
 //import { BrowserRouter, Routes, Route, createBrowserRouter } from "react-router";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Layout from "./Layout";
-import ErrorPage from "./Components/ErrorPage";
-import Todos from "./Components/Todos";
-import SingleEmployee from "./Components/SingleEmployee";
-import EmployeesTable from "./Components/EmployeesTable";
+
+//----------------Create Browser Router v6.4+ -----------------
+// import Home from "./Components/Home";
+// import About from "./Components/About";
+// import { createBrowserRouter, RouterProvider } from "react-router";
+// import Layout from "./Layout";
+// import ErrorPage from "./Components/ErrorPage";
+// import Todos from "./Components/Todos";
+// import SingleEmployee from "./Components/SingleEmployee";
+// import EmployeesTable from "./Components/EmployeesTable";
+//------------------------------------------------------------
+
 // function App() {
 //   return (
 //     <BrowserRouter>
@@ -18,46 +22,82 @@ import EmployeesTable from "./Components/EmployeesTable";
 //     </BrowserRouter>
 //   );
 //}
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        index: true,
-        element: <Home />,
-      },
-      // {
-      //   path: "EmployeeDataGrid",
-      //   element: <EmployeeDataGrid />,
-      // },
-      {
-        path: "employeesTable",
-        element: <EmployeesTable />,
-      },
-      {
-        path: "employees/:id",
-        element: <SingleEmployee />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "todos",
-        element: <Todos />,
-      },
-      {
-        path: "*",
-        element: <ErrorPage />,
-      },
-    ],
-  },
-]);
+//----------------Create Browser Router v6.4+ -----------------
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "/",
+//         index: true,
+//         element: <Home />,
+//       },
+//       // {
+//       //   path: "EmployeeDataGrid",
+//       //   element: <EmployeeDataGrid />,
+//       // },
+//       {
+//         path: "employeesTable",
+//         element: <EmployeesTable />,
+//       },
+//       {
+//         path: "employees/:id",
+//         element: <SingleEmployee />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "todos",
+//         element: <Todos />,
+//       },
+//       {
+//         path: "*",
+//         element: <ErrorPage />,
+//       },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
+//----------------Create Hash Router v6.4+ for GitHub-----------------
+// import "./App.css";
+// import { BrowserRouter, Routes, Route, createBrowserRouter } from "react-router";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Layout from "./Layout";
+import ErrorPage from "./Components/ErrorPage";
+import Todos from "./Components/Todos";
+import SingleEmployee from "./Components/SingleEmployee";
+import EmployeesTable from "./Components/EmployeesTable";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="EmployeeDataGrid" element={<EmployeeDataGrid />} /> */}
+          <Route path="employeesTable" element={<EmployeesTable />} />
+          <Route path="employees/:id" element={<SingleEmployee />} />
+          <Route path="about" element={<About />} />
+          <Route path="todos" element={<Todos />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
+//---------------------------------------------------------------
